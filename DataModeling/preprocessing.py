158c3ae362cd56ec3b260691942a3bd00fa254c2
김희찬 <caphile98@gmail.com>
2023-04-15 13:02:40 +0900

    재료 빈도수 취합

--- DataModeling/preprocessing.py
+++ DataModeling/preprocessing.py
@@ -3,40 +3,56 @@
 from collections import Counter
 import pandas as pd
 import numpy as np
-from tkinter import filedialog
+from tkinter import filedialog, Tk
 import ast
 import os
 import re
 
-def fileOpen():
-    global fileName
-    fileName = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("*.xlsx","*xlsx"), ("*.xls","*xls"), ("*.csv","*csv"), ("all files", "*.*")))
+def fileOpen(opt):
+    root = Tk()
+    root.withdraw()
 
-    df = pd.read_excel(fileName)
-    dt = df.values.tolist()
-    return dt
+    if opt == 1:    # 엑셀파일 한개
+        fileName = filedialog.askopenfilename(title ='Select Excel File', initialdir = '/', filetypes = (('Excel Files', '*.xlsx'), ('All Files', '*.*')))
+        df = pd.read_excel(fileName)
+        dt = df.values.tolist()
+    if opt == 2:    # 여러 파일 선택
+        dt = []
+        fileName = filedialog.askopenfilenames(title ='Select Excel Files', initialdir = '/', filetypes = (('Excel Files', '*.xlsx'), ('All Files', '*.*')))
+        for f in fileName:
+            df = pd.read_excel(f)
+            dt.append(df.values.tolist())
+    
+    root.destroy()
+    return dt, fileName
 
-def fileCreate(dt, val):
-    global fileName
-
-    try:
-        folder = '10000recipeData'  # 엑셀 저장 폴더명
-        os.mkdir(folder)
-    except:
-        pass
+def fileCreate(dt, val, fileName):
+    #try:
+    #    folder = '10000recipeData'  # 엑셀 저장 폴더명
+    #    os.mkdir(folder)
+    #except:
+    #    pass
 
     pattern = r'F\d+_T\d+'
     fName = re.findall(pattern, fileName)[0]
 
-    fName = f'{folder}/{fName}_{val}.xlsx'
+    fName = f'{fName}_{val}.xlsx'
 
-    cols = [['Key', '요리명', '인분', '소요시간', '난이도', '재료', '조리법'], ['재료', '빈도수']]
+    cols = [['Key', '요리명', '인분', '소요시간', '난이도', '재료', '조리법'],
+            ['재료', '빈도수'],
+            ['재료', '빈도수']]
     df = pd.DataFrame(dt, columns = cols[val - 1])
+
+    root = Tk()
+    root.withdraw()
+
+    fName = filedialog.asksaveasfilename(initialdir = '/', initialfile = fName)
+    root.destroy()
 
     df.to_excel(fName, index = False)
 
-def process1():
-    dt = fileOpen()
+def process1(): # 한번에 여러 파일 읽도록 수정 예정
+    dt, fName = fileOpen(1)
 
     subDt = []
     for i in dt:
@@ -67,21 +83,49 @@ def process1():
     for i in range(len(counts_key)):
         param.append([counts_key[i], counts_val[i]])  
     
-    fileCreate(param, 2)
+    fileCreate(param, 2, fName)
 
 def process2():
-    pass
+    dt, fNames = fileOpen(2)
+
+    ingreds = []
+    counts = []
+    for d in dt:
+        d = np.array(d).transpose().tolist()
+        for i, c in zip(d[0], d[1]):
+            if i in ingreds:
+                idx = ingreds.index(i)
+                counts[idx] += int(c)
+            else:
+                ingreds.append(i)
+                counts.append(int(c))
+
+    df = []
+    for i in range(len(ingreds)):
+        df.append([ingreds[i], counts[i]])
+    df = sorted(df, key=lambda x: x[1], reverse=True)
+
+    pattern = r'F\d+_T\d+'
+    fromMin = float('inf')
+    toMax = float('-inf')
+    for f in fNames:
+        nums = re.findall('\d+', re.findall(pattern, f)[0])
+        fromMin = min(fromMin, int(nums[0]))
+        toMax = max(toMax, int(nums[0]))
+
+    title = f'F{fromMin}_T{toMax}'
+
+    fileCreate(df, 3, title)
 
 print('1. 소요시간, 난이도 필터링 및 재료 빈도수')
 print('2. 재료 빈도수 취합')
-
+print('선택 :', end = ' ')
 n = int(input())
 
 while 1:
-    try:
-        if n == 1:
-            process1()
-        if n == 2:
-            process2()
-    except:
+    if n == 1:
+        process1()
+    else:
         break
+if n == 2:
+    process2()
