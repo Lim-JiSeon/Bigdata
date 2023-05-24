ad1a0e03a10df16afe5f3071b1391ea51bdf46ae
김희찬 <caphile98@gmail.com>
2023-05-24 09:10:26 +0900

    엑셀 읽고 쓰는 기능 추가

--- DataModeling/utils.py
+++ DataModeling/utils.py
@@ -1,52 +1,30 @@
 from tkinter import filedialog, Tk
-from konlpy.tag import Komoran
+import pandas as pd
 import os, re
 
 class Normalize:    # 정규화 함수
     def __init__(self):
-        self.stopwords = readFile(os.getcwd() + '/DataModeling', 'Stopwords')
+        self.stopwords = readFile(os.getcwd(), 'Stopwords')
         for i in range(len(self.stopwords)):
             self.stopwords[i] = self.stopwords[i].replace('\n', '')
-        self.komoran = Komoran()
 
     def process(self, text):
         text = self.stripSCharacter(text)
-        if text == '':
-            return text
         text = self.removeStopword(text)
-        if text == '':
-            return text
-        text = self.lowercase(text)
-        if text == '':
-            return text
-        #text = self.tagging(text)
+        #text = self.lowercase(text)
         return text
 
     def stripSCharacter(self, text):        # 특수문자 제거
-        return re.sub('[^ㄱ-ㅎ가-힣a-zA-Z0-9\s]', '', text)
+        #return re.sub('[^a-zA-Z0-9\s]', '', text)
+        return re.sub('[^a-zA-Z0-9+\s\'&-]', '', text)  # +, -, ', & 기호 살림
 
     def removeStopword(self, text):         # 불용어 제거
         words = text.split()
-        newWords = []
-        for word in words:
-            keep = True
-            for s in self.stopwords:
-                if word.find(s) != -1:
-                    keep = False
-                    break
-            if keep:
-                newWords.append(word)
-        return ' '.join(newWords)
-        #return ' '.join([word for word in newWords if word.lower() not in self.stopwords])
+        return ' '.join([word for word in words if word not in self.stopwords])
 
     def lowercase(self, text):              # 소문자화
         words = text.split()
         return ' '.join([word.lower() for word in words])
-
-    def tagging(self, text):
-        words = text.split()
-        return ' '.join([word for word in words if self.komoran.pos(word)[0][1] == 'NNP'])
-               
 
 def filePaths(opt = 1):
     root = Tk()
@@ -55,7 +33,7 @@ def filePaths(opt = 1):
     if opt == 1:
         fullPaths = filedialog.askopenfilenames(title = 'Select txt Files', initialdir = os.getcwd(), filetypes = [("Text files", "*.txt"), ("All files", "*.*")])
     elif opt == 2:
-        fullPaths = filedialog.askopenfilenames(title = 'Select Excel File', initialdir = os.getcwd(), filetypes=[('Excel files', '*.xlsx'), ("All files", "*.*")])
+        fullPaths = filedialog.askopenfilenames(title = 'Select Excel File', initialdir = os.getcwd(), filetypes=[('Excel files',('*.csv', '*.xlsx')), ("All files", "*.*")])
 
     paths, names = [], []
     for p in fullPaths:
@@ -65,14 +43,22 @@ def filePaths(opt = 1):
     
     return paths, names
 
-def readFile(path, name):
-    with open(f'{path}/{name}.txt', 'r', encoding ='UTF8') as f:
-        text = f.readlines()
-    for i in range(len(text)):
-        text[i] = text[i].replace('\n', '')
-    return text
+def readFile(path, name, opt = 1):
+    if opt == 1:
+        with open(f'{path}/{name}', 'r', encoding ='UTF8') as f:
+            data = f.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].replace('\n', '')
+    elif opt == 2:
+        data = pd.read_excel(f'{path}/{name}')
 
-def saveFile(path, name, text):
-    with open(f'{path}/{name}.txt', 'w', encoding ='UTF8') as f:    # txt파일
-        for line in text:
-            f.write(line + '\n')
+    return data
+
+def saveFile(path, name, data, opt = 1, cols = None):
+    if opt == 1:
+        with open(f'{path}/{name}', 'w', encoding ='UTF8') as f:
+            for line in data:
+                f.write(line + '\n')
+    elif opt == 2:
+        df = pd.DataFrame(data, columns = cols)
+        df.to_excel(f'{path}/{name}', index = False)
