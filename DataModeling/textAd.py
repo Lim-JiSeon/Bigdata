dabdea1735213d0cf4709356feb8785ad219ee3a
김희찬 <caphile98@gmail.com>
2023-05-24 10:43:01 +0900

    변수 명 변경

--- DataModeling/textAd.py
+++ DataModeling/textAd.py
@@ -31,6 +31,7 @@ with open(fileName, 'r+', encoding ='cp949') as f:
                 lines.remove(word)
                 print(f'{Fore.LIGHTBLACK_EX}{word}{Style.RESET_ALL}이 목록에서 {Fore.RED}삭제{Style.RESET_ALL}됨\n')
             else:
+                print('\n')
                 continue
         else:
             lines.append(word)
