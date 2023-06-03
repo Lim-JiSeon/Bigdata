b5614967cecd5e5fe1e8473ef7a30a7f140f2b44
김희찬 <caphile98@gmail.com>
2023-06-03 23:33:06 +0900

    spell 기능 검증 필요

--- DataModeling/recipe_spell.py
+++ DataModeling/recipe_spell.py
@@ -1,15 +1,31 @@
-import os
-import sys
-import urllib.request
-import json
-import kss
-from kss import split_sentences
-import pandas as pd
-import numpy as np
+#import os
+#import sys
+#import urllib.request
+#import json
+#import kss
+#from kss import split_sentences
+#import pandas as pd
+#import numpy as np
 import re
 from hanspell import spell_checker
 from kiwipiepy import Kiwi
 
+kiwi = Kiwi()
+
+a = ['1. 일단 고기 500g 정도양에 맛간장1,후추약간,청주1,생강즙1,마늘,참기름 이렇게 넣고 조물조물해준뒤 체에 받혀서 냉장고에 넣어두어요. ', '2. 달구어진팬에 포도씨유를 조금 두르고 고기를 볶아주세요 ', '3. 고기가 익으면. 굴소스로 간을 맞추시는데요. 그 다음 꿀마늘도 넣어줍니다. ', '4. 고기가 볶아지면 버섯과 고추를 넣고 볶아주세요.. 이때 굴소스를 조금 더 넣어서 간을 맞춰주심 됩니다 ', '5. 마지막으로 참기름 과 통깨 뿌리고 마무리하시면 됩니다. ']
+
+def process(recipe):
+    for line in recipe:
+        line = re.sub('[^ㄱ-ㅎ가-힣a-zA-Z0-9\s]', '', line)
+
+        res = kiwi.split_into_sents(line)
+        ress = spell_checker.check(res[0].text)
+
+        print(ress)
+
+process(a)
+
+'''
 ### py-hanspell 설치 오류 해결 및 실행 방법
 # git clone https://github.com/ssut/py-hanspell
 # cd py-hanspell
@@ -39,8 +55,6 @@ df = df.dropna(axis=0)
 # df['조리법_맞춤법'] = spell_checker.check(str(df['조리법']))
 # print(df['조리법'])
 # print(df['조리법_맞춤법'])
-
-
 
 
 # 문장 분리(띄어쓰기) 및 맞춤법 교정
@@ -75,7 +89,4 @@ for i in range(0, len(df['조리법'])):
 # print(df['조리법_spell'])
 df.to_csv('C:/Users/ksc/Desktop/음식레시피_전처리/조리법_교정.csv', encoding='utf-8', index = None)
 # df.to_excel("C:/Users/ksc/Desktop/음식레시피_전처리/조리법_교정.xlsx", encoding = 'utf-8')
-
-
-
-
+'''
