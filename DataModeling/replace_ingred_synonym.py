b39588c235bc3de2ea1c216adc1c14a843a30cc8
jude7794 <jude7794@naver.com>
2023-06-06 04:17:26 +0900

    ingred_synonym.py '속성' 컬럼 표시

--- DataModeling/replace_ingred_synonym.py
+++ DataModeling/replace_ingred_synonym.py
@@ -5,6 +5,7 @@ from soynlp.tokenizer import RegexTokenizer
 from gensim.models import word2vec
 
 def replace_similar_words(text):
+    # 직접 학습시킨 word2vec 모델 불러오기
     model = word2vec.Word2Vec.load('word2vec_syn.model')
 
     # 재료 한 번에 입력 가능(\n으로 구분) ex) 소금\n양파\n설탕\n다진마늘\n참기름
